0d1e465b76c9ad5ac1cf3e2cdce74b5a23773b12
OlehMarch <oleh.marchenko.95@gmail.com>
2019-12-07 15:48:06 +0200

    initial commit, added proj

---
+++ internet_shop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace internet_shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
