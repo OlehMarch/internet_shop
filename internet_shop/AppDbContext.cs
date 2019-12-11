00b6983c69db774ab87e15282fc61f0a3c6339aa
Oleksandr Riznichenko <riznichenko.o@gmail.com>
2019-12-11 10:42:19 +0100

    Create AppDbContext.cs
    Connect to DB

---
+++ internet_shop/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbName = "shop";
+            string dbUser = "root";
+            string dbHost = "localhost";
+
+            optionsBuilder.UseMySql(
+                $"server={dbHost};UserId={dbUser};database={dbName};"
+                );
+        }
+        public DbSet<Brand> Brand { get; set; }
+    }
+}
