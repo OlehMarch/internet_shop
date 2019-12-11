790a7c597d725cfb1d7e6c2ed51fbfa5f5c41b2e
Oleksandr Riznichenko <riznichenko.o@gmail.com>
2019-12-11 10:26:15 +0100

    Create Brand.cs
    Create model brand

---
+++ internet_shop/Models/Brand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shop.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        //public List<Promos> promo {get; set}
+    }
+}
