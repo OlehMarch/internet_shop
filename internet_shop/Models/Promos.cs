c18ecbd7e84f78b7f8e776c0163000841975ab3f
toredosss <toredo2016@gmail.com>
2019-12-11 10:57:03 +0200

    Create Promos.cs
    create promos

---
+++ internet_shop/Models/Promos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace internet_shop.Models
+{
+    public class Promos
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+}
