91df71b27f799f5db92c218e2d67395e745bd8b3
Panmcpon <panmcpon999@gmail.com>
2019-12-11 10:25:15 +0200

    Categories
    done)
    
    (cherry picked from commit a7be459a27a2fef98abf68fe182aa7da0085f425)

---
+++ internet_shop/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using internet_shop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace internet_shop
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+            //Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbName = "internet_shop";
+            string dbUser = "root";
+            string dbHost = "localhost";
+
+            optionsBuilder.UseMySql(
+                $"server={dbHost};UserId={dbUser};database={dbName};"
+                );
+        }
+        public DbSet<Categories> Cat { get; set; }
+    }
+}
---
+++ internet_shop/Controllers/CategoriesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using internet_shop.Services;
+using internet_shop.Models;
+
+namespace internet_shop.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private CategoriesService _catService;
+        public CategoriesController(CategoriesService catService)
+        {
+            _catService = catService;
+        }
+        // GET: api/Cat
+        [HttpGet]
+        public List<Categories> Get()
+        {
+            return _catService.GetAllCategory();
+            
+        }
+
+        // GET: api/Cat/5
+        [HttpGet("{id}", Name = "Get")]
+        public Categories Get(int id)
+        {
+            var cat = _catService.GetCategoryById(id);
+            return cat;
+        }
+
+        // POST: api/Cat
+        [HttpPost]
+        public IActionResult AddNewCategory([FromBody] Categories categories)
+        {
+            var category = _catService.AddCategories(categories.Name,categories.Value);
+            if (category == false)
+                return BadRequest("Bad request");
+            else
+                return Ok("Code 200, new category is added");
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("/delete")]
+        public IActionResult Delete(int id)
+        {
+            var category = _catService.DeleteCategoryById(id);
+            if (category == false)
+                return BadRequest("404");
+            else
+                return Ok("200");
+        }
+    }
+}
