360d17875f219dbfb15e05d4872a11d4832ae2e0
Oleksandr Riznichenko <riznichenko.o@gmail.com>
2019-12-13 14:28:29 +0100

    Change BrandController+BrandService

---
+++ internet_shop/Controllers/BrandController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+using internet_shop.Models;
+using internet_shop.Services;
+
+namespace internet_shop.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly BrandService _brandService;
+
+        public BrandController(BrandService brandService)
+        {
+            _brandService = brandService;
+        }
+        // GET: api/Brand
+        [HttpGet]
+        public List<Brand> Get()
+        {
+            return _brandService.GetAllBrand();
+        }
+
+        // GET: api/Brand/5
+        [HttpGet("{id}")]
+
+        public Brand GetById(int id)
+        {
+            var brand = _brandService.GetBrandById(id);
+            return brand;
+        }
+
+        [HttpGet("ProductById")]
+        public List<Product> GetAllProductByBrand([FromBody] Product product)
+        {
+            var products = _brandService.GetProductsByBrand(product.BrandId);
+            return products;
+        }
+
+        // POST: Brand/Create
+        [HttpPost]
+        public IActionResult AddNewBrand([FromBody] Brand brand)
+        {
+            var brands = _brandService.AddBrand(brand.Name);
+
+            if (brands == false)
+            {
+                return BadRequest("This is bad request");
+            }
+            else
+            {
+                return Ok("Ok, new brand added");
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("/delete")]
+        public ActionResult Delete(int id)
+        {
+            var brands = _brandService.DeleteBrandById(id);
+            if (brands == false)
+                return BadRequest("404");
+            else
+                return Ok("200");
+        }
+    }
+}
---
+++ internet_shop/Services/BrandService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Microsoft.EntityFrameworkCore;
+
+using internet_shop.Models;
+using internet_shop.DbContexts;
+
+namespace internet_shop.Services
+{
+    public class BrandService
+    {
+        public BrandService(BrandDbContext db, ProductDbContext dbProduct)
+        {
+            _db = db;
+            _dbProduct = dbProduct;
+
+        }
+
+        private readonly BrandDbContext _db;
+        private readonly ProductDbContext _dbProduct;
+        private DbSet<Brand> Brand => _db.Brand;
+        private DbSet<Product> _Products => _dbProduct.Products;
+
+        public List<Brand> GetAllBrand()
+        {
+            return Brand.ToList();
+        }
+
+        public List<Product> GetProductsByBrand(int id)
+        {
+            var products = _Products.Where((x) => x.BrandId == id).ToList();
+            if (products == null || products.Count == 0)
+                return null;
+            else
+                return products;
+        }
+
+        public Brand GetBrandById(int id)
+        {
+            var brand = Brand.SingleOrDefault((Brand brand) => brand.Id == id);
+            if (brand == null)
+            {
+                return null;
+            }
+            return brand;
+        }
+        public bool DeleteBrandById(int id)
+        {
+            Brand brand = _Brand.SingleOrDefault((Brand brand) => brand.Id == id);
+
+            if (brand == null)
+            {
+                return false;
+            }
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public bool AddBrand(string name)
+        {
+            Brand brand = ToEntity(name);
+            _Brand.Add(brand);
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public Brand ToEntity(string name)
+        {
+            return new Brand
+            {
+                Name = name
+            };
+        }
+    }
+}
