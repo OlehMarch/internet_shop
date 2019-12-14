58a701114bb10c0dc337a155c79d3c4302a47409
toredosss <toredo2016@gmail.com>
2019-12-14 13:23:43 +0200

    Add PromosDto
    Add PromosDto

---
+++ internet_shop/Dto/PromosDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace internet_shop.Dto
+{
+    public class PromosDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UniversalId { get; set; }
+        public int Value { get; set; }
+        public int ProductId { get; set; }
+        public int BrandId { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
